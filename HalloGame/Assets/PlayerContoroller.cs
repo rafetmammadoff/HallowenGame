433cc64ef948eb8de13e5cfdce39788fe4fa9b8f
Rafet <professionalraf@gmail.com>
2022-10-29 09:39:24 +0400

    CharacterMoveWithSplineComputers

--- HalloGame/Assets/PlayerContoroller.cs
+++ HalloGame/Assets/PlayerContoroller.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Dreamteck;
 using Dreamteck.Splines;
+using DG.Tweening;
 
 
 
@@ -16,7 +17,8 @@ public class PlayerContoroller : MonoBehaviour
     [SerializeField] private SplineComputer _left;
     [SerializeField] private SplineComputer _middle;
     [SerializeField] private SplineComputer _right;
-
+    [SerializeField] private int _counter = 0;
+    Sequence seq = DOTween.Sequence();
 
 
     void Start()
@@ -30,25 +32,73 @@ public class PlayerContoroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Horizontal = Input.GetAxisRaw("Horizontal");
-       
-        transform.position+=new Vector3(Horizontal,0,1)*speed*Time.deltaTime;
-        if (Horizontal < 0)
+        Horizontal = Input.GetAxis("Horizontal");
+
+        //transform.position+=new Vector3(Horizontal,0,1)*speed*Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
         {
+
             SplineFollower splineFollower = transform.GetComponent<SplineFollower>();
-            double startPoint = splineFollower.startPosition;
-            splineFollower.spline = _left;
-            splineFollower.startPosition = startPoint;
+            if (splineFollower.spline == _middle)
+            {
+                var seq = DOTween.Sequence();
+                double startPoint = splineFollower.startPosition;
+                Vector3 leftDistance = new Vector3(-5.5f, 0, 4.1f);
+                seq.Append(transform.DOMove((transform.position + leftDistance), 0.5f)).AppendCallback(() =>
+                {
+                    splineFollower.spline = _left;
+                    splineFollower.startPosition = startPoint;
+                });
+
+
+
+            }
+            if (splineFollower.spline == _right)
+            {
+                var seq = DOTween.Sequence();
+                double startPoint = splineFollower.startPosition;
+                Vector3 leftDistance = new Vector3(-5.5f, 0, 4.1f);
+                seq.Append(transform.DOMove((transform.position + leftDistance), 0.5f)).AppendCallback(() =>
+                {
+                    splineFollower.spline = _middle;
+                    splineFollower.startPosition = startPoint;
+                });
+
+
+            }
+        }
+            if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+            {
+                SplineFollower splineFollower = transform.GetComponent<SplineFollower>();               
+                if (splineFollower.spline == _middle)
+                {
+                    var seq = DOTween.Sequence();
+                    double startPoint = splineFollower.startPosition;
+                    Vector3 rightDistance = new Vector3(5.5f, 0, 4.1f);
+                    seq.Append(transform.DOMove((transform.position + rightDistance), 0.5f)).AppendCallback(() =>
+                    {
+                       splineFollower.spline = _right;
+                        splineFollower.startPosition = startPoint;
+                    });
+                }
+                    if (splineFollower.spline == _left)
+                    {
+                        var seq = DOTween.Sequence();                   
+                        double startPoint = splineFollower.startPosition;
+                        Vector3 rightDistance = new Vector3(5.5f, 0, 4.1f);
+                        seq.Append(transform.DOMove((transform.position + rightDistance), 0.5f)).AppendCallback(() =>
+                        {
+                            splineFollower.spline = _middle;
+                            splineFollower.startPosition = startPoint;
+                        });
+
+
+                    }
+
+                }
+            }
+        }
+    
+
+
             
-        }
-        if (Horizontal > 0)
-        {
-            SplineFollower splineFollower = transform.GetComponent<SplineFollower>();
-            double startPoint = splineFollower.startPosition;
-            splineFollower.spline = _right;
-            splineFollower.startPosition = startPoint;
-
-        }
-
-    }
-}
